44eac69e1df8e1f334a64f9125c4e00675fa5ea0
xp-rodion <only.learning.ariy@gmail.com>
2023-02-26 16:08:38 +0300

    redact: add error handler

--- treemenu/templatetags/draw_menu.py
+++ treemenu/templatetags/draw_menu.py
@@ -6,5 +6,9 @@ register = template.Library()
 
 @register.inclusion_tag('tags/menu.html')
 def draw_menu(slug, req_item=None):
-    menu = Menu.objects.prefetch_related('item_menu').get(slug=slug)
-    return {'menu': menu, 'req_item': req_item}
+    try:
+        menu = Menu.objects.prefetch_related('item_menu').get(slug=slug)
+        context = {'menu': menu, 'req_item': req_item}
+    except Menu.DoesNotExist:
+        context = {'menu': None, 'req_item': req_item}
+    return context
